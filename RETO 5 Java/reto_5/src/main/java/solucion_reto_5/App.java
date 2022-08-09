13f1856f10f5c1e8dc4f727902d092aff09f6551
onfevs <jorgevallejo2990@gmail.com>
2022-08-09 16:56:24 -0500

    Reto 5
    Solución reto 5

---
+++ RETO 5 Java/reto_5/src/main/java/solucion_reto_5/App.java
@@ -0,0 +1,13 @@
+package solucion_reto_5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
